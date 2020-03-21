c71fc9690a5b621126dd0c99f5e6c5b3741a69ba
Jenny Ardrey <jenny@ardrey.co.uk>
2020-03-21 16:43:40 +0000

    basic project layout and components

---
+++ src/ContactForm.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./ContactForm.css"
+
+function ContactForm() {
+	return (
+		<div className="contact-form">
+			<p>contactform</p>
+		</div>
+	)
+}
+
+export default ContactForm
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Navbar.css"
+
+
+function Navbar() {
+	return (
+		<div className="nav-bar">
+			<p>nav</p>
+		</div>
+	)
+}
+
+export default Navbar
